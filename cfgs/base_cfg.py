50c150c4be3ad335677020590decfe55f778aff2
qxchen <qxchencoding@outlook.com>
2022-07-12 15:52:34 +0800

    Update: modify base_cfg

--- cfgs/base_cfg.py
+++ cfgs/base_cfg.py
@@ -1,94 +1,94 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class CommonCfg(object):
-    def __init__(self) -> None:
-        self.name = None  # required
-        self.is_training = None
-        self.toy = False
-        self.toy_size = 5
-        self.cudnn_benchmark = False
-        self.data_dir = None
-        self.gpu_ids = -1
-        self.rand_seed = 42
-        self.save_dir = None
-        self.resutls_dir = None
+        name = None  # required
+        is_training = None
+        toy = False
+        toy_size = 5
+        cudnn_benchmark = False
+        data_dir = None
+        gpu_ids = -1
+        rand_seed = 42
+        save_dir = None
+        resutls_dir = None
 
 
 # Write fundamental configurations in the class.
 # Usually these cfgs do not change.
+@dataclass
 class IMGCfg(CommonCfg):
-    def __init__(self) -> None:
-        super().__init__()
-        self.series = 'sagittal'
-        self.pe_types = ['central', 'segmental']
-        self.hide_probability = 0
-        self.hide_level = 'window'
-        self.only_topmost_window = False
-        self.pkl_path = None    # required
-        self.img_format = 'raw'
-        self.dataset = None     # required
-        self.patience = 10
-        self.best_ckpt_metric = 'val_loss'
-        self.max_eval = -1
-        self.max_ckpts = 2
+        series = 'sagittal'
+        pe_types = ['central', 'segmental']
+        hide_probability = 0
+        hide_level = 'window'
+        only_topmost_window = False
+        pkl_path = None    # required
+        img_format = 'raw'
+        dataset = None     # required
+        patience = 10
+        best_ckpt_metric = 'val_loss'
+        max_eval = -1
+        max_ckpts = 2
 
-        self.use_pretrained = False
-        self.include_normals = False
-        self.use_hem = False
-        self.fine_tune = True
-        self.fine_tuning_lr = 0
-        self.fine_tuning_boundary = 'encoder.3'
-        self.hidden_dim = 32
-        self.elastic_transform = False
-        self.do_hflip = True
-        self.do_vflip = False
-        self.do_rotate = True
-        self.do_jitter = True
-        self.do_center_pe = True
-        self.abnormal_prob = 0.5
-        self.resize_shape = [224, 224]  # '224, 224'
-        self.crop_shape = [208, 208]    # '208, 208'
-        self.min_abnormal_slices = 4
+        use_pretrained = False
+        include_normals = False
+        use_hem = False
+        fine_tune = True
+        fine_tuning_lr = 0
+        fine_tuning_boundary = 'encoder.3'
+        hidden_dim = 32
+        elastic_transform = False
+        do_hflip = True
+        do_vflip = False
+        do_rotate = True
+        do_jitter = True
+        do_center_pe = True
+        abnormal_prob = 0.5
+        resize_shape = [224, 224]  # '224, 224'
+        crop_shape = [208, 208]    # '208, 208'
+        min_abnormal_slices = 4
 
-        self.num_workers = 4
-        self.vstep_size = 1
+        num_workers = 4
+        vstep_size = 1
 
-        self.model = 'PENet'
-        self.ckpt_path = None
-        self.num_channels = 3
-        self.num_classes = 1
-        self.num_slices = 32
-        self.batch_size = 8
-        self.init_method = 'kaiming'
-        self.model_depth = 50
+        model = 'PENet'
+        ckpt_path = None
+        num_channels = 3
+        num_classes = 1
+        num_slices = 32
+        batch_size = 8
+        init_method = 'kaiming'
+        model_depth = 50
 
-        self.agg_method = 'max'
-        self.threshold_size = 0
+        agg_method = 'max'
+        threshold_size = 0
 
-        self.eval_mode = 'series'
-        self.do_classify = False
-        self.num_visuals = 4
+        eval_mode = 'series'
+        do_classify = False
+        num_visuals = 4
 
-        self.visualize_all = False
+        visualize_all = False
 
-
+@dataclass
 class EHRCfg(CommonCfg):
-    def __init__(self) -> None:
-        super().__init__()
-        self.dataset = None
-        self.ckpt_path = None
+        dataset = None
+        ckpt_path = None
 
-        self.num_classes = 1
-        self.batch_size = 16
-        self.num_epochs = 1
+        num_classes = 1
+        batch_size = 16
+        num_epochs = 1
 
-        self.optimizer = 'sgd'
-        self.learning_rate = 0.01
+        optimizer = 'sgd'
+        learning_rate = 0.01
         # SGD
-        self.sgd_momentum = 0.9
-        self.sdg_dampening = 0.9
+        sgd_momentum = 0.9
+        sdg_dampening = 0.9
         # Adam
-        self.adam_beta_1 = 0.9
-        self.adam_beta_2 = 0.999
+        adam_beta_1 = 0.9
+        adam_beta_2 = 0.999
         # Regularization
-        self.dropout_prob = 0
-        self.l1_lambda = 0.1
-        self.weight_decay = 0.001
+        dropout_prob = 0
+        l1_lambda = 0.1
+        weight_decay = 0.001
